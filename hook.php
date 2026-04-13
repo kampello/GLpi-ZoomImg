94cdc5629f51aed4a678f1cef07609909c16575e
Campello <a89790@ualg.pt>
2026-04-13 10:51:55 +0100

    hook added

---
+++ hook.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Função executada ao clicar em 'Instalar'
+ */
+function plugin_zoomimg_install() {
+   return true;
+}
+
+/**
+ * Função executada ao clicar em 'Desinstalar'
+ */
+function plugin_zoomimg_uninstall() {
+   return true;
+}
